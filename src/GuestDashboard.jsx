7737e86f23ce8a565da38802100fc5ae3a496cbe
Aditya sahu <saditya23cs@student.mes.ac.in>
2026-01-27 21:41:22 +0530

    Your commit message

--- src/GuestDashboard.jsx
+++ src/GuestDashboard.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { API_BASE_URL } from "./config";
+import "./GuestDashboard.css";
 
 export default function GuestDashboard() {
   const navigate = useNavigate();
